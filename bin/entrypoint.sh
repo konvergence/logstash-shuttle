a1fb81f66b6b8c1b78c229c1af08b9eba4d69037
Slaheddinne Ahmed <slaheddinne.ahmed@koeos.lan>
2018-04-04 15:10:30 +0200

    v1.0.1

---
+++ bin/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+if [ "$1" = "--help" ]
+then
+	echo "hello  this is help me file \n please refer to the image maintainer (slaheddinne)  "
+else
+	apt-get update
+	apt-get install -y gettext-base
+	if [ $1 = "--run" ] && [ $OUTPUT_ONLY = "true" ]
+	then
+		envsubst < /config-dir/debug_shuttle-"$SHUTTLE_VERSION".conf.dist > /config-dir/logstash.conf
+		#if [ $DEBUG ] 
+	elif [ $1 = "--run" ] && [ $OUTPUT_ONLY = "false" ]
+	then
+		envsubst < /config-dir/shuttle-"$SHUTTLE_VERSION".conf.dist > /config-dir/logstash.conf
+	fi
+
+#cat /config-dir/$LOGSTASH_CONF
+	logstash $LOGSTASH_OPTIONS -f /config-dir/logstash.conf
+
+fi	
